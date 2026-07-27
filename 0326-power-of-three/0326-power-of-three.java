7cba43864463a2832572d6f845101d965b3b95d5
Sarthak Hota <galaxygaming0511@gmail.com>
2026-07-27 14:39:46 +0530

    07-27-2026 9 ms (9.62%) 46.1 MB (26.23%) 0326-power-of-three Easy .java 07-27-2026

---
+++ 0326-power-of-three/0326-power-of-three.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if (n <= 0) return false;
+
+        long x = 1;
+        while (x < n) {
+            x *= 3;
+        }
+        return x == n;
+    }
+}
